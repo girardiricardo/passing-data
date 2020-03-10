e843ece4f59917a8a623d47b34300735958159e8
Ricardo Girardi <girardiricardo@outlook.com>
2020-03-10 02:15:28 +0000

    Displaying favorite movies of the users list

--- src/App.js
+++ src/App.js
@@ -106,6 +106,15 @@ class App extends Component {
           <h1 className="App-title">ReactND - Coding Practice</h1>
         </header>
         <h2>Favorite Movies</h2>
+		<ol>
+		{profiles.map((profile) => {
+        	return (
+				<li key={profile.id}>
+                  {users[profile.userID].name}'s favorite movie is {movies[profile.favoriteMovieID].name}.
+          		</li>
+          	)
+        })}
+		</ol>
       </div>
     );
   }
